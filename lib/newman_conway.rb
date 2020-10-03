211e1dd33847cec7863b6bbe742836154c95961d
Alicia Combs <alicia.combs.92@gmail.com>
2020-10-03 00:17:40 -0700

    Finished wave 1, pass all tests

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,26 @@
-
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(1)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError.new("Parameter must be a positive int") if num == 0
+
+  number_hash = { 1 => 1, 2 => 1 }
+
+  nc_helper(num, number_hash)
+
+  pattern_string = ""
+
+  (1..num).each do |i|
+    pattern_string += number_hash[i].to_s
+    pattern_string += " "
+  end
+
+  pattern_string.chop
+end
+
+def nc_helper(n, hash)
+  if hash[n]
+    hash[n]
+  else
+    hash[n] = nc_helper(nc_helper(n - 1, hash), hash) + nc_helper(n - nc_helper(n - 1, hash), hash); 
+  end
 end
